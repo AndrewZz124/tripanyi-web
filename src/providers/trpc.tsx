b8b27038350fa4dde282dd2a08c68c300b85dd5b
Zheng Zhang <156475082+AndrewZz124@users.noreply.github.com>
2026-06-17 15:52:10 -0400

    Update trpc.tsx

--- src/providers/trpc.tsx
+++ src/providers/trpc.tsx
@@ -2,12 +2,13 @@ import { createTRPCReact } from "@trpc/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import superjson from "superjson";
-import type { AppRouter } from "../../api/router";
 import type { ReactNode } from "react";
 
-export const trpc = createTRPCReact<AppRouter>();
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const trpc = createTRPCReact<any>();
 
 const queryClient = new QueryClient();
+
 const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
